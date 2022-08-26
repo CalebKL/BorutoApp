46733fce06162733a8127f779c9fb6c7dea7ac79
Mzazi25 <langat.caleb25@gmail.com>
2022-08-26 07:59:18 +0300

    Created Database converter to convert lists to strings and viceversa

--- app/src/main/java/com/example/burutoapp/data/local/HeroDatabase.kt
+++ app/src/main/java/com/example/burutoapp/data/local/HeroDatabase.kt
@@ -2,12 +2,14 @@ package com.example.burutoapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.burutoapp.data.local.dao.HeroDao
 import com.example.burutoapp.data.local.dao.HeroRemoteKeyDao
 import com.example.burutoapp.domain.models.Hero
 import com.example.burutoapp.domain.models.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase: RoomDatabase() {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
