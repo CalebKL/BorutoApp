c39835aa1958b29ebc14f257958a4b61e8ee046f
Mzazi25 <langat.caleb25@gmail.com>
2022-08-29 22:13:07 +0300

    Created function to inject hilt to the datastore functionality

---
+++ app/src/main/java/com/example/burutoapp/di/RepositoryModule.kt
@@ -0,0 +1,25 @@
+package com.example.burutoapp.di
+
+import android.content.Context
+import com.example.burutoapp.data.preference.DataStoreOperationImp
+import com.example.burutoapp.domain.repository.DataStoreOperations
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule{
+    @Provides
+    @Singleton
+    fun provideDataStoreOperation(
+        @ApplicationContext context: Context): DataStoreOperations {
+        return DataStoreOperationImp(
+            context = context
+        )
+
+    }
+}
