9a0867fb8b7e2873116ff8294bd0c87bf4e55760
Mzazi25 <langat.caleb25@gmail.com>
2022-08-30 15:39:26 +0300

    Created WelcomeViewModel

---
+++ app/src/main/java/com/example/burutoapp/presentation/welcome/WelcomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.burutoapp.presentation.welcome
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.burutoapp.domain.use_case.UseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WelcomeViewModel @Inject constructor(
+    private val useCases: UseCases
+):ViewModel() {
+    fun saveOnBoardingState(completed:Boolean){
+        viewModelScope.launch(Dispatchers.IO) {
+            useCases.saveOnBoardingUseCase(completed)
+        }
+    }
+}
