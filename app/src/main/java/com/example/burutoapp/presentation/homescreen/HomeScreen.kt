784daca5a7e73389603e084dda623e77a959b2a3
Mzazi25 <langat.caleb25@gmail.com>
2022-09-02 09:05:51 +0300

    Updated homescreen

--- app/src/main/java/com/example/burutoapp/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/burutoapp/presentation/homescreen/HomeScreen.kt
@@ -2,10 +2,16 @@ package com.example.burutoapp.presentation.homescreen
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.burutoapp.presentation.homescreen.components.HomeTopAppBar
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
     Scaffold(
         topBar = {
             HomeTopAppBar(onSearchClicked = {})
