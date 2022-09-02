4faac1ac55af3440eb1ac2df833f48ed9fbce960
Mzazi25 <langat.caleb25@gmail.com>
2022-09-02 05:47:56 +0300

    specified how to provide an instance of remoteDataSource

--- app/src/main/java/com/example/burutoapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/burutoapp/di/NetworkModule.kt
@@ -1,6 +1,10 @@
 package com.example.burutoapp.di
 
+import androidx.paging.ExperimentalPagingApi
+import com.example.burutoapp.data.local.HeroDatabase
 import com.example.burutoapp.data.remote.BorutoApi
+import com.example.burutoapp.data.repository.RemoteDataSourceImp
+import com.example.burutoapp.domain.repository.RemoteDataSource
 import com.example.burutoapp.util.Constants.BASE_URL
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
@@ -14,6 +18,7 @@ import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
+@ExperimentalPagingApi
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
@@ -43,5 +48,17 @@ object NetworkModule {
     @Singleton
     fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
         return retrofit.create(BorutoApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(
+        borutoApi: BorutoApi,
+        heroDatabase: HeroDatabase
+    ): RemoteDataSource{
+        return RemoteDataSourceImp(
+            heroDatabase = heroDatabase,
+            borutoApi = borutoApi
+        )
     }
 }
