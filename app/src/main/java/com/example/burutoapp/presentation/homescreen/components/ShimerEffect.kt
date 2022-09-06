9809c784d78d64f52baff8a6f5422143c3bc1c2f
Mzazi25 <langat.caleb25@gmail.com>
2022-09-06 10:23:15 +0300

    Created Shimmer Effect

---
+++ app/src/main/java/com/example/burutoapp/presentation/homescreen/components/ShimerEffect.kt
@@ -0,0 +1,79 @@
+package com.example.burutoapp.presentation.homescreen.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.burutoapp.presentation.theme.*
+
+@Composable
+fun ShimmerEffect() {
+
+}
+
+@Composable
+fun ShimmerItem() {
+   Surface(
+       modifier = Modifier
+           .fillMaxWidth()
+           .height(HERO_ITEM_HEIGHT),
+       color = if (isSystemInDarkTheme())
+           Color.Black else shimmerLightGray,
+       shape = RoundedCornerShape(LARGE_PADDING)
+   ) {
+       Column(
+           modifier = Modifier
+               .padding(all = MEDIUM_PADDING),
+           verticalArrangement = Arrangement.Bottom
+       ){
+           Surface(
+               modifier = Modifier
+                   .fillMaxWidth(0.5f)
+                   .height(SHIMMER_HEIGHT),
+               color = if (isSystemInDarkTheme())
+                   shimmerDarkGray else shimmerMediumGray,
+               shape = RoundedCornerShape(size = SMALL_PADDING)
+           ){
+           }
+       }
+    }
+}
+
+
+@Preview
+@Composable
+fun ShimmerItemPreview() {
+    ShimmerItem()
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun ShimmerItemPreview2() {
+    ShimmerItem()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/burutoapp/presentation/theme/Color.kt
+++ app/src/main/java/com/example/burutoapp/presentation/theme/Color.kt
@@ -12,7 +12,9 @@ val Teal200 = Color(0xFF03DAC5)
 val LightGray = Color(0xFFD8D8D8)
 val DarkGray = Color(0xFF2A2A2A)
 val StarColor = Color(0xFFFFC94D)
-
+val shimmerLightGray = Color(0xFFF1F1F1)
+val shimmerMediumGray = Color(0xFFE3E3E3)
+val shimmerDarkGray = Color(0xFF1D1D1D)
 val Colors.welcomeScreenBackground
 @Composable
 get() = if (isLight) Color.White else DarkGray
