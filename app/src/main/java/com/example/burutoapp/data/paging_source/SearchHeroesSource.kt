2eddc180fbb650cbb42c0e435102c62a99d82f90
Mzazi25 <langat.caleb25@gmail.com>
2022-09-12 14:04:42 +0300

    Created load function on Paging3 library

--- app/src/main/java/com/example/burutoapp/data/paging_source/SearchHeroesSource.kt
+++ app/src/main/java/com/example/burutoapp/data/paging_source/SearchHeroesSource.kt
@@ -2,14 +2,39 @@ package com.example.burutoapp.data.paging_source
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.example.burutoapp.data.remote.BorutoApi
 import com.example.burutoapp.domain.models.Hero
+import javax.inject.Inject
 
-class SearchHeroesSource: PagingSource<Int, Hero>(){
-    override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
-
-    }
+class SearchHeroesSource @Inject constructor(
+    private val borutoApi: BorutoApi,
+    private val query: String
+): PagingSource<Int, Hero>(){
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Hero> {
+        return try {
+            val apiResponse = borutoApi.searchHeroById(name = query)
+            val heroes =apiResponse.heroes
+            if (heroes.isNotEmpty()){
+                LoadResult.Page(
+                    data = heroes,
+                    prevKey = apiResponse.prevPage,
+                    nextKey = apiResponse.nextPage
+                )
+            }else{
+                LoadResult.Page(
+                    data = emptyList(),
+                    prevKey = null,
+                    nextKey = null
+                )
+            }
+        }catch (e:Exception){
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
         TODO("Not yet implemented")
+
     }
 }
