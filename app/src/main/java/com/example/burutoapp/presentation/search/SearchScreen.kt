d9bb0a21a16e36a2725b8ce03939fb500ecb7a06
Mzazi25 <langat.caleb25@gmail.com>
2022-09-12 10:02:30 +0300

    Created SearchTopBar and SearchWidget Composables

--- app/src/main/java/com/example/burutoapp/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/burutoapp/presentation/search/SearchScreen.kt
@@ -1,8 +1,14 @@
 package com.example.burutoapp.presentation.search
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SearchScreen() {
-
+    Scaffold(
+        topBar = {
+            SearchTopBar()
+        }
+    ){}
 }
---
+++ app/src/main/java/com/example/burutoapp/presentation/search/SearchTopBar.kt
@@ -0,0 +1,23 @@
+package com.example.burutoapp.presentation.search
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchTopBar(
+    text: String,
+    onTextChanged:(String)-> Unit,
+    onSearchedClicked:(String)->Unit,
+    onCloseClicked:(String)->Unit
+){
+
+}
+
+@Composable
+fun SearchWidget(
+    text: String,
+    onTextChanged:(String)-> Unit,
+    onSearchedClicked:(String)->Unit,
+    onCloseClicked:(String)->Unit
+){
+
+}
