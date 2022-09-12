e03e26868c0048ec26e720ccb225279984d96760
Mzazi25 <langat.caleb25@gmail.com>
2022-09-12 15:16:39 +0300

    Reused Screen Content to search for heroes and displayed list of searched heroes

--- app/src/main/java/com/example/burutoapp/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/burutoapp/presentation/search/SearchScreen.kt
@@ -6,14 +6,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+import com.example.burutoapp.presentation.common.ScreenContent
+import com.example.burutoapp.presentation.homescreen.HomeViewModel
 
+@ExperimentalCoilApi
 @Composable
 fun SearchScreen(
-    navController: NavHostController,
-    searchViewModel: SearchViewModel = hiltViewModel()
+    navController: NavHostController
 ) {
+    val searchViewModel= hiltViewModel<SearchViewModel>()
     val searchQuery by searchViewModel.searchQuery
-
+    val heroes = searchViewModel.searchedHeroes.collectAsLazyPagingItems()
     Scaffold(
         topBar = {
             SearchTopBar(
@@ -21,11 +26,16 @@ fun SearchScreen(
                 onTextChanged = {
                     searchViewModel.updateSearchQuery(query = it)
                 },
-                onSearchedClicked = {},
+                onSearchedClicked = {
+                    searchViewModel.searchedHeroes(query = it)
+                },
                 onCloseClicked = {
                     navController.popBackStack()
                 }
             )
+        },
+        content = {
+            ScreenContent(navController = navController, heroes = heroes)
         }
-    ){}
+    )
 }
--- app/src/main/java/com/example/burutoapp/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/burutoapp/presentation/search/SearchViewModel.kt
@@ -2,13 +2,37 @@ package com.example.burutoapp.presentation.search
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.burutoapp.domain.models.Hero
+import com.example.burutoapp.domain.use_case.UseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SearchViewModel:ViewModel() {
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val useCases: UseCases
+):ViewModel() {
+    private val _searchedHeroes = MutableStateFlow<PagingData<Hero>>(PagingData.empty())
+    val searchedHeroes = _searchedHeroes
 
     private val _searchQuery = mutableStateOf("")
     val searchQuery = _searchQuery
 
     fun updateSearchQuery(query:String){
         _searchQuery.value = query
+    }
+
+    fun searchedHeroes(query: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            useCases.searchHeroesUseCase(query = query).cachedIn(viewModelScope).collect {
+                _searchedHeroes.value = it
+            }
+        }
     }
 }
