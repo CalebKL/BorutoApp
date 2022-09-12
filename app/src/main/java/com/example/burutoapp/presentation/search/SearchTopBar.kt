d5b64d8d22a0f1f2502bf181ed398b6e40223371
Mzazi25 <langat.caleb25@gmail.com>
2022-09-12 10:21:27 +0300

    Updated TextField Search AppBar

--- app/src/main/java/com/example/burutoapp/presentation/search/SearchTopBar.kt
+++ app/src/main/java/com/example/burutoapp/presentation/search/SearchTopBar.kt
@@ -1,6 +1,18 @@
 package com.example.burutoapp.presentation.search
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.burutoapp.R
+import com.example.burutoapp.presentation.theme.TOP_APP_BAR_HEIGHT
+import com.example.burutoapp.presentation.theme.topAppBarBackgroundColor
+import com.example.burutoapp.presentation.theme.topAppBarContentColor
 
 @Composable
 fun SearchTopBar(
@@ -19,5 +31,25 @@ fun SearchWidget(
     onSearchedClicked:(String)->Unit,
     onCloseClicked:(String)->Unit
 ){
-
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBackgroundColor
+    ){
+        TextField(
+            value = text,
+            onValueChange ={
+                onTextChanged(it)
+        }, 
+            placeholder = {
+                Text(
+                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    text = stringResource(R.string.search_hero),
+                    color = Color.White
+                )
+            }
+        )
+    }
 }
