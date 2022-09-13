d2e3db9b3e511d4f7ea1b8a93703947206bd8ec4
Mzazi25 <langat.caleb25@gmail.com>
2022-09-13 09:14:48 +0300

    Enabled Swipe to refresh

--- app/src/main/java/com/example/burutoapp/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/example/burutoapp/presentation/common/EmptyScreen.kt
@@ -20,14 +20,21 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import com.example.burutoapp.R
+import com.example.burutoapp.domain.models.Hero
 import com.example.burutoapp.presentation.theme.ICON_SIZE
 import com.example.burutoapp.presentation.theme.SMALL_PADDING
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 
 @Composable
-fun EmptyScreen(error: LoadState.Error? = null) {
+fun EmptyScreen(
+    error: LoadState.Error? = null,
+    heroes: LazyPagingItems<Hero>? = null
+) {
     var message by remember{ mutableStateOf("Find your Favourite Hero")}
     var icon by remember { mutableStateOf(R.drawable.ic_search_document)}
 
@@ -46,37 +53,57 @@ fun EmptyScreen(error: LoadState.Error? = null) {
     LaunchedEffect(key1 = true){
         startAnimation = true
     }
-    EmptyContent(alphaAnim = alphaAnim, icon = icon, message = message)
+    EmptyContent(
+        alphaAnim = alphaAnim,
+        icon = icon,
+        message = message,
+        heroes = heroes,
+        error = error
+    )
 }
 
 @Composable
 fun EmptyContent(
+    error: LoadState.Error? = null,
     alphaAnim: Float,
     icon: Int,
     message: String,
+    heroes: LazyPagingItems<Hero>? = null
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Icon(
-            modifier = Modifier
-                .size(ICON_SIZE)
-                .alpha(alpha = alphaAnim),
-            painter = painterResource(id = icon),
-            contentDescription = stringResource(id = R.string.error_icon),
-            tint = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
-        )
-        Spacer(modifier = Modifier.height(SMALL_PADDING))
-        Text(
-            modifier = Modifier.alpha(alpha = alphaAnim),
-            text = message,
-            color= if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray,
-            textAlign= TextAlign.Center,
-            fontWeight = FontWeight.Medium,
-            style = MaterialTheme.typography.subtitle1,
-        )
+    var isRefreshing by remember { mutableStateOf(false) }
+
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = isRefreshing ),
+        swipeEnabled = error != null,
+        onRefresh = {
+            isRefreshing = true
+            heroes?.refresh()
+            isRefreshing = false
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Icon(
+                modifier = Modifier
+                    .size(ICON_SIZE)
+                    .alpha(alpha = alphaAnim),
+                painter = painterResource(id = icon),
+                contentDescription = stringResource(id = R.string.error_icon),
+                tint = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+            )
+            Spacer(modifier = Modifier.height(SMALL_PADDING))
+            Text(
+                modifier = Modifier.alpha(alpha = alphaAnim),
+                text = message,
+                color= if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray,
+                textAlign= TextAlign.Center,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.subtitle1,
+            )
+        }
     }
 }
 
