4ce6369dd6922e1c0afc0d030c120ad3db24c27b
Mzazi25 <langat.caleb25@gmail.com>
2022-09-14 06:57:42 +0300

    created function to getSelected use case to our repository

--- app/src/main/java/com/example/burutoapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/burutoapp/data/repository/Repository.kt
@@ -3,11 +3,13 @@ package com.example.burutoapp.data.repository
 import androidx.paging.PagingData
 import com.example.burutoapp.domain.models.Hero
 import com.example.burutoapp.domain.repository.DataStoreOperations
+import com.example.burutoapp.domain.repository.LocalDataSource
 import com.example.burutoapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
+    private val local: LocalDataSource,
     private val remote: RemoteDataSource,
     private val dataStoreOperations: DataStoreOperations
 ) {
@@ -16,6 +18,10 @@ class Repository @Inject constructor(
     }
     fun searchHeroes(query:String): Flow<PagingData<Hero>>{
         return remote.searchHeroes(query = query)
+    }
+
+    suspend fun getSelectedHero(heroId:Int): Hero{
+        return local.getSelectedHero(heroId = heroId)
     }
     suspend fun saveOnBoardingState(completed:Boolean){
         dataStoreOperations.saveOnBoardingState(completed = completed)
